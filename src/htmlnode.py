d1be85e45df028884f3283d84c53e3d4ff3d81da
rhruban <rhruban@gmail.com>
2025-10-01 11:11:20 -0600

    added parentnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,7 +18,7 @@ class HTMLNode:
 
     def __eq__(self,other):
         return (
-            self.tag == other.tag 
+            self.tag == other.tag
             and self.value == other.value 
             and self.children == other.children
             and self.props == other.props
@@ -26,3 +26,19 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent Node needs a tag")
+        if self.children in None:
+            raise ValueError("Parent Node needs child")
+        htmlout = f"<{self.tag}{self.props_to_html()}>"
+        for child in children:
+            htmlout += child.to_html()
+        htmlout += f"</{self.tag}>"
+        return htmlout
