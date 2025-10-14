ba624958d66a5d11b28ac08a8e287286fd4df31e
rhruban <rhruban@gmail.com>
2025-10-14 13:08:45 -0600

    added fn textnode to leafnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -23,5 +24,20 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise ValueError(f"invalid text type: {text_node.text_type}")
+    if text_node.text_type is TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type is TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type is TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type is TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type is TextType.LINK:
+        return LeafNode("a", text_node.text, {"href":text_node.url})
+    if text_node.text_type is TextType.IMAGE:
+        return LeafNode("img", "", {"src":text_node.url,"alt":text_node.text})
 
 
