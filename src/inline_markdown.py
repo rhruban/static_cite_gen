032a8c8ff6c542bbb4aac5f27334e0a2058da91a
rhruban <rhruban@gmail.com>
2025-10-24 14:34:58 -0600

    added splitting inline markdown

---
+++ src/inline_markdown.py
@@ -0,0 +1,24 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            chunks = node.text.split(delimiter)
+            if len(chunks) % 2 != 1:
+                raise ValueError("Markdown syntax error, formatted section not closed")
+            count = 0
+            for chunk in chunks:
+                count += 1
+                if chunk == '':
+                    continue
+                if count % 2 == 1:
+                    new_nodes.append(TextNode(chunk, TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(chunk, text_type))
+    return new_nodes
+
+
+
