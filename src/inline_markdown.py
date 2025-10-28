7318c018364b5c3dce94cc352ad2ee4911be930a
rhruban <rhruban@gmail.com>
2025-10-28 14:44:01 -0600

    added splitting text with images and links into nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -30,3 +30,47 @@ def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
 
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        images = extract_markdown_images(old_node.text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        texts = old_node.text
+        for image in images:
+            parts = texts.split(f"![{image[0]}]({image[1]})", 1)
+            if parts[0] != '':
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0],TextType.IMAGE,image[1]))
+            texts = parts[1]
+        if texts != '':
+            new_nodes.append(TextNode(texts, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        images = extract_markdown_links(old_node.text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        texts = old_node.text
+        for image in images:
+            parts = texts.split(f"[{image[0]}]({image[1]})", 1)
+            if parts[0] != '':
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0],TextType.LINK,image[1]))
+            texts = parts[1]
+        if texts != '':
+            new_nodes.append(TextNode(texts, TextType.TEXT))
+    return new_nodes
+
+
+
